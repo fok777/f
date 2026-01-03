fcdcc3f0865ac9d7dda11d98842d8d3cb0bee132
ApartTUSITU <aptusitu@outlook.com>
2026-01-03 14:27:20 +0800

    Enable UFS LPM for all devices. (Follow Xiaomi upstream code)
    Ref: https://github.com/MiCode/Xiaomi_Kernel_OpenSource/blob/cmi-r-oss/drivers/scsi/ufs/ufs-qcom.c#L2033-L2038

--- drivers/scsi/ufs/ufs-qcom.c
+++ drivers/scsi/ufs/ufs-qcom.c
@@ -2033,12 +2033,11 @@ __setup("androidboot.bootdevice=", get_android_boot_dev);
  */
 static void ufs_qcom_parse_lpm(struct ufs_qcom_host *host)
 {
-#if IS_ENABLED(CONFIG_MACH_XIAOMI_UMI) || IS_ENABLED(CONFIG_MACH_XIAOMI_THYME)
-	host->disable_lpm = false;
-#else
+#ifdef CONFIG_BOARD_XIAOMI
 	struct device_node *node = host->hba->dev->of_node;
-
 	host->disable_lpm = of_property_read_bool(node, "qcom,disable-lpm");
+#else
+	host->disable_lpm = false;
 #endif
 	if (host->disable_lpm)
 		pr_info("%s: will disable all LPM modes\n", __func__);
